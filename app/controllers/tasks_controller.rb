ec4184c22c050d4230c35830a424836cd87be319
nissymimi <nissym325@gmail.com>
2019-05-25 06:49:58 +0000

    retry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,26 +19,27 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
      
    if @task.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'タスクを投稿しました。'
       redirect_to root_url
     else
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
-      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
       render 'toppages/index'
    end
   end
 
     
-    def edit
+  def edit
       @task = Task.find(params[:id])
-    end
+      
+  end
     
   def update
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_path
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
